49d1910e5d51e80dd57b0c61d8e4d0a7eff67a62
chairielazizi <azizihr2000@gmail.com>
2020-08-09 15:56:17 +0800

    add golang_notes.go

---
+++ golang_notes.go
@@ -0,0 +1,230 @@
+package main
+
+import (
+	"fmt"
+	"strconv" // to cast number to string
+)
+
+// if we declare at package level, cannot assign using :=
+// must use
+// var i int = 3
+
+// can also intitialze like this
+var (
+	actorName  string = "Kristen Stewart"
+	actorAge   int32  = 28
+	actorHobby string = "Eating"
+)
+
+// visibilty of variable in golang
+// lowercase naming :	var i int	--> if package level, any file in the package can access it
+// uppercase naming :	var I int	--> exported from the package and globally visible
+// block scope		:	--> not visible to the outside block itself
+
+func main() {
+	fmt.Println("First programe in GoLang")
+
+	var num1 int = 55 //intialization
+	num2 := 65        //declare and initialization(one time only)
+	var result int = num1 + num2
+	fmt.Println(result)
+
+	//declare and assign value
+	var number int //declaration
+	number = 4
+	fmt.Println("The number is", number)
+
+	//type casting
+	number4 := float32(number)
+	fmt.Printf("%v is now %T\n", number4, number4)
+	//cast int to string, use "strconv" package
+	number4string := strconv.Itoa(number)
+	fmt.Printf("%v is now %T\n", number4string, number4string)
+
+	var i, j = 100, "hello"
+	fmt.Printf("i is %d and j is %s\n", i, j)
+	var aa1 float32 = 35
+	fmt.Printf("aa1 is %v and type %T\n", aa1, aa1)
+	//fmt.Println("i is " + i + "\nj is " + j)
+	// will cause compile time error if variables were declared and not used in program
+
+	// summary of variables in GoLang
+	// - cant redeclare variables, but can shadow them
+	// - all variables must be used
+	// - visibilty: - lowercase first leeter for packgae scope
+	// 			 - uppercase first letter to export
+	// 			 - no private scope
+	// - declaration:	- var foo int
+	// 				- var foo int = 43
+	// 				- foo := 43
+	// - type conversions: - destinationType(variable)
+	// 					- use strconv package for strings
+
+	// 2nd chapter: Primitives
+
+	// boolean
+	var goku bool = true
+	fmt.Printf("\n%v, %T\n", goku, goku)
+	// check equality
+	// initially declaration variable is zero value
+	goku1 := 1 == 1
+	goku2 := 1 == 2
+	fmt.Printf("%v, %T\n", goku1, goku1)
+	fmt.Printf("%v, %T\n", goku2, goku2)
+	// integer Primitives
+	// uint16, uint32
+	goku3 := 10 //  1010
+	goku4 := 3  //	0011
+	// arithmetic operator
+	fmt.Println("Arithmetic operator")
+	fmt.Println(goku3 + goku4)
+	fmt.Println(goku3 - goku4)
+	fmt.Println(goku3 * goku4)
+	fmt.Println(goku3 / goku4)
+	fmt.Println(goku3 % goku4)
+	// bitwise operator
+	fmt.Println("Bitwise operator")
+	fmt.Println(goku3 & goku4)  // 0010
+	fmt.Println(goku3 | goku4)  // 1011
+	fmt.Println(goku3 ^ goku4)  // 1001
+	fmt.Println(goku3 &^ goku4) // 0100
+	fmt.Println("Shifting")
+	// shifting bitwise
+	goku5 := 8              // 2^3
+	fmt.Println(goku5 << 3) //	2^3 * 2*3 = 2^6
+	fmt.Println(goku5 >> 3) //	2^3 / 2^3 = 2^0
+	//	floating point number
+	goku6 := 3.14
+	goku6 = 13.e72
+	goku6 = 2.1E14
+	fmt.Printf("%v, %T\n", goku6, goku6)
+
+	goku7 := 10.2
+	goku8 := 3.7
+	fmt.Println(goku7 + goku8)
+	fmt.Println(goku7 - goku8)
+	fmt.Println(goku7 * goku8)
+	fmt.Println(goku7 / goku8)
+	// fmt.Println(goku7 % goku8)	no % operator for float
+
+	// complex number : complex 64, complex 128
+	fmt.Println("\nComplex number")
+	var goku9 complex64 = 1 + 2i
+	fmt.Printf("%v, %T\n", goku9, goku9)
+	var goku10 complex64 = 2 + 5.2i
+	fmt.Println(goku9 + goku10)
+	fmt.Println(goku9 - goku10)
+	fmt.Println(goku9 * goku10)
+	fmt.Println(goku9 / goku10)
+	fmt.Printf("%v, %T\n", real(goku9), real(goku9))
+	fmt.Printf("%v, %T\n", imag(goku9), imag(goku9))
+	// can also initialize like this
+	var goku11 complex128 = complex(5, 12)
+	fmt.Printf("%v, %T\n", goku11, goku11)
+
+	// text data type
+	s1 := "this is a string"
+	fmt.Printf("%v, %T\n", s1, s1)
+	fmt.Printf("%v, %T\n", s1[2], s1[2]) // letter i in s1
+	// s1[2] = "u"
+	// fmt.Printf("%v, %T\n", s1, s1)  cause error
+
+	// string concat
+	s2 := " just another string"
+	fmt.Printf("%v, %T\n", s1+s2, s1+s2)
+
+	// convert to slice of bytes
+	// because many func in golang use slice of bytes
+	// flexible than hardcoded string
+	b1 := []byte(s1)
+	fmt.Printf("%v, %T\n", b1, b1)
+
+	// rune
+	// rune is type alias for int32
+	// r1 := 'a'
+	var r1 rune = 'a'
+	fmt.Printf("%v, %T\n", r1, r1)
+
+	// summary of Primitive data type in golang
+	// - boolean type: - true or false
+	// 				- not an alias for other types
+	// 				- zero value is false
+	// - numeric types: - integers: signed int
+	// 				 - unsigned integers
+	// 				 - arithmetic operations
+	// 				 - bitwise operations
+	// 				 - zero value is 0
+	// 				 - cannot mix types in same family (int32 + int 64 = error!)
+
+	// 				 - floating point:
+	// 				 - follow ieee-754 standard
+	// 				 - 32 and 64 bit version
+	// 				 - decimal(3.14) , exponential(1e5), mixed(1.1e5)
+
+	// 				 - complex number:
+	// 				 - zero value is (0 +0i)
+	// 				 - 64 and 128 bit version
+	// 				 - complex, real, imag built-in func
+
+	// 				 - text types:-
+	// 				 - strings:
+	// 				 - UTF-8
+	// 				 - immutable
+	// 				 - can be concatenated with +
+
+	// 				 - rune:
+	// 				 - UTF-32
+	// 				 - alias for int32
+	// 				 - special methods normally required to process
+	// 				 - strings.Reader.#ReadRune
+
+	// 3rd chapter: Constants
+	//for declaring constant
+	const ba = 99
+	fmt.Println("\n", ba)
+
+	// for loop type 1
+	var k int
+	fmt.Println("\nFor loop type 1")
+	for k = 1; k <= 10; k += 2 {
+		fmt.Println(k)
+	}
+
+	//if else statement
+	var max int = 100
+	if max < 10 {
+		fmt.Println(max, "is less than 10")
+	} else if max > 10 && max < 100 {
+		fmt.Println(max, "is between 10 and 100")
+	} else {
+		fmt.Println(max, "is equal or bigger than 100")
+	}
+
+	//switch statement
+	a, b := 1, 2
+	switch a + b {
+	case 1:
+		fmt.Println("The sum is 1")
+	case 2:
+		{
+			fmt.Println("The sum is 2")
+		}
+	case 3:
+		fmt.Println("The sum is 3")
+	case 4:
+		fmt.Println("The sum is 4")
+	default:
+		fmt.Println("Printing default")
+	}
+
+	//array in goland
+	var array1 [5]string
+	array1[0] = "My"
+	array1[1] = "name"
+	array1[2] = "is"
+	array1[3] = "Chairiel"
+	array1[4] = "Azizi"
+	for k = 0; k < len(array1); k++ {
+		fmt.Printf(array1[k] + " ")
+	}
+}
